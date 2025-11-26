6a8de6c673604aa5fccd29626e73d9716168e27c
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-11-26 16:34:50 +0800

    fix k6 workflow (5)

--- frontend/performance/k6.config.ts
+++ frontend/performance/k6.config.ts
@@ -1,3 +1,7 @@
+declare var __ENV: {
+  [key: string]: string | undefined;
+};
+
 export const TestEnvConfig = {
   BASE_URL: __ENV.BASE_URL || "http://localhost:8080/api",
   USERNAME: __ENV.USERNAME || "testuser",
@@ -5,7 +9,7 @@ export const TestEnvConfig = {
 };
 
 // @ts-expect-error
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/v3.0.3/dist/bundle.js";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/3.0.3/dist/bundle.js";
 // @ts-expect-error
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
 
